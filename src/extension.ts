cd87e7937311dad5b045ba663b21f196ab47c88b
hassamullah23 <malikhassamullah1@yahoo.com>
2022-06-16 05:54:54 -0700

    image added

--- src/extension.ts
+++ src/extension.ts
@@ -13,13 +13,19 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('line-counter.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('line-counter.sayHello', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello from line-counter by Hassam Ullah!');
 	});
+	let disposable2 = vscode.commands.registerCommand('line-counter.sayHello2', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello 2 from line-counter by Hassam Ullah!');
+	});
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable2);
 }
 
 // this method is called when your extension is deactivated
